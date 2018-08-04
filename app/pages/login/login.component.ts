0e80942a3d62ebe1a21edaeba05c99dcec062cc3
Sepehr <sepehrsadri@gmail.com>
2018-08-04 19:35:43 +0430

    + hint color android and ios api aded but have bugs

--- app/pages/login/login.component.ts
+++ app/pages/login/login.component.ts
@@ -3,8 +3,10 @@ import { Router } from "@angular/router";
 import { Color } from "color";
 import { View } from "ui/core/view";
 import { Page } from 'ui/page';
+import { TextField } from "ui/text-field";
 import { User } from '../../shared/user/user';
 import { UserService } from "../../shared/user/user.service";
+import { setHintColor } from '../../utils/hint-util';
 
 
 
@@ -18,6 +20,8 @@ export class LoginComponent implements OnInit {
 	user: User;
 	isLoggingIn = true;
 	@ViewChild("container") container: ElementRef;
+	@ViewChild("email") email: ElementRef;
+	@ViewChild("password") password: ElementRef;
 	ngOnInit() {
 		this.page.actionBarHidden = true;
 		this.page.backgroundImage = "res://bg_login";
@@ -61,11 +65,24 @@ export class LoginComponent implements OnInit {
 	}
 	toggleDisplay() {
 		this.isLoggingIn = !this.isLoggingIn;
+		this.setTextFieldColors();
 		let view = <View>this.container.nativeElement;
 		view.animate({
 			backgroundColor: this.isLoggingIn ? new Color("white") : new Color("#301217"),
 			duration: 200
 		});
+	}
+	setTextFieldColors() {
+		let emailTextField = <TextField>this.email.nativeElement;
+		let passwordTextField = <TextField>this.password.nativeElement;
+
+		let mainTextColor = new Color(this.isLoggingIn ? "black" : "#C4AFB4");
+		emailTextField.color = mainTextColor;
+		passwordTextField.color = mainTextColor;
+
+		let hintColor = new Color(this.isLoggingIn ? "#ACA6A7" : "#C4AFB4");
+		setHintColor({ view: emailTextField, color: hintColor });
+		setHintColor({ view: passwordTextField, color: hintColor });
 	}
 
 
