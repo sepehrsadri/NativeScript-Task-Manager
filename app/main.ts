97e7d374775d6a3b5680ad1e3df60c9145b2b7fe
Sepehr <sepehrsadri@gmail.com>
2018-08-05 01:13:08 +0430

    + statusBar translucent add

--- app/main.ts
+++ app/main.ts
@@ -1,5 +1,6 @@
 import { platformNativeScriptDynamic } from "nativescript-angular/platform";
 import { AppModule } from "./app.module";
+import { setStatusBarColors } from './utils/status-bar-util';
 
-
+setStatusBarColors();
 platformNativeScriptDynamic().bootstrapModule(AppModule);
