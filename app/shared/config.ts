e16e4d0a81a87b48504d6c0e37459f4c7a450ab7
Sepehr <sepehrsadri@gmail.com>
2018-09-15 17:14:52 +0430

    + ip fix

--- app/shared/config.ts
+++ app/shared/config.ts
@@ -1,5 +1,5 @@
 export class Config {
-	static apiUrl = "http://192.168.43.243:8081/";
+	static apiUrl = "http://192.168.1.40:8081/";
 	static appKey = "kid_HyHoT_REf";
 	static authHeader = "Basic a2lkX0h5SG9UX1JFZjo1MTkxMDJlZWFhMzQ0MzMyODFjN2MyODM3MGQ5OTIzMQ";
 	static token = "";
