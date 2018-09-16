c0df89c971031519eeafdf07c209b340eede45fb
Sepehr <sepehrsadri@gmail.com>
2018-09-16 23:00:19 +0430

    + ip fix and navigate back icon remove

--- app/shared/grocery/grocery.service.ts
+++ app/shared/grocery/grocery.service.ts
@@ -14,7 +14,7 @@ export class GroceryService {
 	// baseUrl = Config.apiUrl + "appdata/" + Config.appKey + "/Groceries";
 	baseWorkUrl = "http://192.168.1.40:8081/grocery/";
 	baseHomeUrl = "http://192.168.43.243:8081/grocery/";
-	baseUrl = this.baseWorkUrl;
+	baseUrl = this.baseHomeUrl;
 
 	constructor(private http: Http, private HTtp: HttpClient) { }
 
